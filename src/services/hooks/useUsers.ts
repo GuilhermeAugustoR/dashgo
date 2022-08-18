db7210126ef55be74d0fae4d182f4e9b864585e0
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2022-08-18 12:50:07 -0300

    ♻️ Trocando a typagem do options

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -6,45 +6,47 @@ type User = {
   name: string;
   email: string;
   createdAt: string;
-}
+};
 
 type GetUsersResponse = {
   totalCount: number;
   users: User[];
-}
+};
 
 export async function getUsers(page: number): Promise<GetUsersResponse> {
-  const { data, headers } = await api.get('users', {
+  const { data, headers } = await api.get("users", {
     params: {
       page,
-    }
-  })
+    },
+  });
 
-  const totalCount = Number(headers['x-total-count'])
+  const totalCount = Number(headers["x-total-count"]);
 
-  const users = data.users.map(user => {
+  const users = data.users.map((user) => {
     return {
       id: user.id,
       name: user.name,
       email: user.email,
-      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric'
-      })
+      createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
     };
   });
 
-  return { 
+  return {
     users,
     totalCount,
   };
 }
 
-export function useUsers(page: number, options: UseQueryOptions) {
-  return useQuery(['users', page], () => getUsers(page), {
+export function useUsers(
+  page: number,
+  options: UseQueryOptions<GetUsersResponse>
+) {
+  return useQuery(["users", page], () => getUsers(page), {
     staleTime: 1000 * 60 * 10, // 10 minutes
     ...options,
-  })
+  });
 }
-
